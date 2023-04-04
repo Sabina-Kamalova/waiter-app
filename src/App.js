e224c84712bbeca93849e8a0a8aea8d97ccc8e7d
Sabina Kamalova <skamalova09@gmail.com>
2023-04-04 13:56:29 +0200

    Add NavBar and data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import NotFound from './components/pages/NotFound/NotFound';
 import Table from './components/pages/Table/Table';
 import Tables from './components/pages/Tables/Tables';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
 
 const App = () => {
   return (
     <Container>
+      <Header />
       <Routes>
         <Route path="/" element={<Tables />} />
         <Route path="/table/:id" element={<Table />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Container>
   )
 }
